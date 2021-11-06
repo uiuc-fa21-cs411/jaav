198c811f5f9dc7b289255c1c212e96f7be59e349
AbhiWangoo <awangoo2@illinois.edu>
2021-11-06 20:25:31 +0000

    Added checkboxes to page

--- JAAV_Project/static/js/script.js
+++ JAAV_Project/static/js/script.js
@@ -3,11 +3,25 @@ var getData
 $( document ).ready(function() {
     plotly_canvas_result_table = document.getElementById('canvas-result-table')
     plotly_canvas_result_plot = document.getElementById('canvas-result-plot')
+    q1_result = document.getElementById('q1')
+    q2_result = document.getElementById('q2')
 
-    getData = function (query_string) {
+    getData = function (query_string, select_query_1, select_query_2) {
         query_string = $("#sql-text-area").val();
+        select_query_1 = 0;
+        select_query_2 = 0;
+
+        if (q1_result.checked) {
+            select_query_1 = 1;
+        }
+        if (q2_result.checked) {
+            select_query_2 = 1;
+        }
+
         $.post( "/query", {
-            query_string: query_string
+            query_string: query_string,
+            select_query_1: select_query_1,
+            select_query_2: select_query_2
         }, function(result, status){
             result_data = result['data']
 
@@ -43,6 +57,6 @@ $( document ).ready(function() {
     }
 
     $("#sendButton").click(function() {
-        getData($("#sql-text-area").val())
+        getData($("#sql-text-area").val(), $("#q1").val(), $("#q2").val())
     })
 })
