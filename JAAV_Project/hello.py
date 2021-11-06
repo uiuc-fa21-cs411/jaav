fa9f8d2605c8d61d26f78b0ad803035a8076188c
Alan Wandke <awandke2@yongjoo-cs411-course-project.q5uqgrs2d4ye5bdt0ts4tjiexf.ex.internal.cloudapp.net>
2021-11-06 20:29:17 +0000

    added comments to sql insertion

--- JAAV_Project/hello.py
+++ JAAV_Project/hello.py
@@ -3,11 +3,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 import mysql.connector
 
-#until a better solution, update these variables before running and before committing
-my_user = "awandke2"
-my_database = "awandke2_database"
-my_password = "Mayaisveryfluffy1!"
-
 @app.route('/')
 def home():
    return render_template('index.html', title = "JAAV Final Project")
@@ -15,14 +10,13 @@ def home():
 @app.route('/query', methods=['POST'])
 def process_query():
     user_in = (request.form['query_string'])
-    test_val = (request.form['test_val'])
-    print(test_val)
+    print(user_in)
     
     mydb = mysql.connector.connect(
     host='localhost',
-    user=my_user,
-    database=my_database,
-    password=my_password)
+    user='awandke2',
+    database='awandke2_database',
+    password='-')
     
     mycursor = mydb.cursor()
 
@@ -62,37 +56,6 @@ def process_query():
     return response
 
 
-@app.route('/insert', methods=['POST'])
-def process_insert():
-    user_in = (request.form['insert_string']).strip()
-    print(user_in)
-
-    mydb = mysql.connector.connect(
-    host='localhost',
-    user=my_user,
-    database=my_database,
-    password=my_password)
-    
-    mycursor = mydb.cursor()
-
-    statement = "insert into FavoriteTrails values (fake_username,'%s',1) "%user_in
-    print(statement)
-    mycursor.execute(statement)
-
-    for (Username, TrailName, Visited) in mycursor:
-        print("{}, {}, {}".format(Username, TrailName, Visited))
-
-    #df = pandas.read_sql_query(sql)
-    mycursor.close()
-    mydb.close()
-
-    def make_valid(v):
-        if v != v:
-            return None
-        else:
-            return v
-        
-    return {}
 
 
 if __name__ == '__main__':
--- JAAV_Project/static/js/script.js
+++ JAAV_Project/static/js/script.js
@@ -5,10 +5,9 @@ $( document ).ready(function() {
     plotly_canvas_result_plot = document.getElementById('canvas-result-plot')
 
     getData = function (query_string) {
-        query_string = $("#query-text-area").val();
+        query_string = $("#sql-text-area").val();
         $.post( "/query", {
-            query_string: query_string,
-            test_val: 23
+            query_string: query_string
         }, function(result, status){
             result_data = result['data']
 
@@ -43,20 +42,7 @@ $( document ).ready(function() {
         }, 'json')
     }
 
-    $("#queryButton").click(function() {
-        getData($("#query-text-area").val())
+    $("#sendButton").click(function() {
+        getData($("#sql-text-area").val())
     })
-
-    insertData = function (insert_string) {
-      query_string = $("#insert-text-area").val();
-      $.post( "/insert", {
-          insert_string: insert_string
-      },function(result, status){
-          result_data = result['data']
-      }, 'json')
-    }
-
-    $("#insertButton").click(function() {
-      insertData($("#insert-text-area").val())
-  })
 })
