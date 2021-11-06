6e675b256ec1874f5f967530a6ef952ddee90919
Vineet Chinthakindi <vineetc2@yongjoo-cs411-course-project.q5uqgrs2d4ye5bdt0ts4tjiexf.ex.internal.cloudapp.net>
2021-11-06 20:50:38 +0000

    fixed query selection issue

--- JAAV_Project/hello.py
+++ JAAV_Project/hello.py
@@ -10,36 +10,36 @@ def home():
 @app.route('/query', methods=['POST'])
 def process_query():
     user_in = (request.form['query_string'])
-    select_q1 = (request.form['select_query_1'])
+    select_q1 = request.form['select_query_1']
     select_q2 = (request.form['select_query_2'])
 
-    print(select_q1)
+    print(type(select_q1))
     print(select_q2)
     print(user_in)
     
     mydb = mysql.connector.connect(
     host='localhost',
-    user='awangoo2',
-    database='awangoo2_database',
+    user='vineetc2',
+    database='vineetc2_database',
     password='')
     
     mycursor = mydb.cursor()
 
-    if select_q1 == 1:
-        query = "SELECT *, count(ParkBiodiversity.Biodiversity) AS NativeCount FROM Parks INNER JOIN ParkBiodiversity ON Parks.ParkName = ParkBiodiversity.Park WHERE Parks.ParkName like concat('%%', '%s', '%%') and ParkBiodiversity.Nativeness = 'Native' GROUP BY ParkName ORDER BY NativeCount desc;"%user_in.strip()
-    elif select_q2 == 1:
-        query = "select * from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
-    else:
-        query = "select * from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
+    query = "select * from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
+    if select_q1 == "1":
+        print("reached")
+        query = "select Parks.ParkName from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
+    elif select_q2 == "1":
+        query = "select Parks.State from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
 
     print(query)
-    mycursor.execute(query)
+    #mycursor.execute(query)
 
     # for (ParkName, Latitude, Longitude, Size, State) in mycursor:
-        # print("{}, {}, {}, {}, {}".format(ParkName, Latitude, Longitude, Size, State))
+    #     print("{}, {}, {}, {}, {}".format(ParkName, Latitude, Longitude, Size, State))
 
     df = pandas.read_sql_query(query, mydb)
-    print(df)
+    print("error")
     mycursor.close()
     mydb.close()
 
