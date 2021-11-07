d24d995d33fbe64c0341601d180042964a09a96a
Janani Rajadurai <jananir2@yongjoo-cs411-course-project.q5uqgrs2d4ye5bdt0ts4tjiexf.ex.internal.cloudapp.net>
2021-11-07 23:23:16 +0000

    Added update trail textbox

--- JAAV_Project/hello.py
+++ JAAV_Project/hello.py
@@ -8,7 +8,7 @@ def home():
    return render_template('index.html', title = "JAAV Final Project")
 
 @app.route('/query', methods=['POST'])
-def process_query():
+def process_query_search():
     user_in = (request.form['query_string'])
     select_q1 = request.form['select_query_1']
     select_q2 = (request.form['select_query_2'])
@@ -19,9 +19,9 @@ def process_query():
     
     mydb = mysql.connector.connect(
     host='localhost',
-    user='vineetc2',
-    database='vineetc2_database',
-    password='')
+    user='jananir2',
+    database='jananir2_database',
+    password='Mseq131640!')
     
     mycursor = mydb.cursor()
 
@@ -37,6 +37,50 @@ def process_query():
 
     # for (ParkName, Latitude, Longitude, Size, State) in mycursor:
     #     print("{}, {}, {}, {}, {}".format(ParkName, Latitude, Longitude, Size, State))
+
+    df = pandas.read_sql_query(query, mydb)
+    print("error")
+    mycursor.close()
+    mydb.close()
+
+    def make_valid(v):
+        if v != v:
+            return None
+        else:
+            return v
+
+    column_labels = [col for col in df.columns]
+    per_col_values = [
+        [make_valid(value) for value in df[col]]
+        for col in df.columns
+    ]
+
+    response = {
+        "query_string": query,
+        "data": {
+            "labels": [[col] for col in column_labels],
+            "values": per_col_values
+        }
+    }
+
+    print(response)
+    return response
+
+@app.route('/query', methods=['POST'])
+def process_query():
+    user_in = (request.form['query_string'])
+
+    print(user_in)
+    
+    mydb = mysql.connector.connect(
+    host='localhost',
+    user='jananir2',
+    database='jananir2_database',
+    password='Mseq131640!')
+    
+    mycursor = mydb.cursor()
+
+    query = "select * from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in.strip()
 
     df = pandas.read_sql_query(query, mydb)
     print("error")
--- JAAV_Project/sql_insertion.py
+++ JAAV_Project/sql_insertion.py
@@ -2,21 +2,21 @@ import mysql.connector
 import csv 
 mydb = mysql.connector.connect(
     host='localhost',
-    user='vineetc2',
-    database='vineetc2_database',
-    password=''
+    user='jananir2',
+    database='jananir2_database',
+    password='Mseq131640!'
 )
 
-with open('/home/vineetc2/data/parks.csv') as csv_file:
+with open('../data/species.csv') as csv_file:
     csv_file = csv.reader(csv_file, delimiter= ',')
     all_value = []
     for row in csv_file:
-        value = (row[1], row[4], row[5], row[3], row[2])
+        value = (row[0], row[1], row[2])
         all_value.append(value)
     
     all_value.pop(0)
 
-query = "insert into `Parks` values(%s, %s,%s,%s, %s)"
+query = "insert into `Species` values(%s, %s,%s)"
 
 
 mycursor = mydb.cursor()
