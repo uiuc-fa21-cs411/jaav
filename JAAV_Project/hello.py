5eaeefd66f3bc3a1eaa508bc113842e22b753937
Alan Wandke <awandke2@yongjoo-cs411-course-project.q5uqgrs2d4ye5bdt0ts4tjiexf.ex.internal.cloudapp.net>
2021-11-09 04:02:38 +0000

    phase 4 complete

--- JAAV_Project/hello.py
+++ JAAV_Project/hello.py
@@ -16,9 +16,15 @@ def process_query():
     username_in = (request.form['update_fav_trail_usrnm']).strip()
     trailname_in = (request.form['update_fav_trail_trlnm']).strip()
     custom_query = (request.form['custom_query']).strip()
+    del_user = (request.form["del_user"]).strip()
+    del_trail = (request.form["del_trail"]).strip()
+    create_user = request.form["create_user"].strip()
+    create_pass = request.form["create_pass"].strip()
+    add_trail_user = request.form["add_trail_user"].strip()
+    add_trail_name = request.form["add_trail_name"].strip()
     # print(type(select_q1))
     # print(select_q2)
-    # print(custom_query)
+  
 
     mydb = mysql.connector.connect(
     host='localhost',
@@ -29,19 +35,8 @@ def process_query():
     mycursor = mydb.cursor()
 
     query = ''
-    # this will execute any sql query, useful for debugging
-    if custom_query != '':
-        if ("select" in custom_query.lower()):
-          query = custom_query
-        else:
-          mycursor.execute(custom_query)
-          mydb.commit()
-          query = '' 
-    # for both output is: ParkName + data, NativeCount, LongTrailPop, ShortTrailPop
-    # output: all park data + native count
-
     # runs general parks query modified by q1 and/or q2
-    elif user_in != '':
+    if user_in != '':
         query = "select * from Parks where Parks.ParkName like concat('%%', '%s', '%%')"%user_in
         if select_q1 == "1" and select_q2 != "1":
           query = "SELECT Parks.ParkName, Parks.Latitude, Parks.Longitude, Parks.Size, Parks.State, count(ParkBiodiversity.Biodiversity) AS NativeCount FROM Parks INNER JOIN ParkBiodiversity ON Parks.ParkName = ParkBiodiversity.Park WHERE ParkBiodiversity.Nativeness = 'Native' and Parks.ParkName like concat('%%', '%s', '%%') GROUP BY ParkName ORDER BY NativeCount desc"%user_in
@@ -51,15 +46,45 @@ def process_query():
           print("hello from else statement")
           query = "SELECT * FROM(SELECT AVG(Trails.Popularity) as longTrailPopularity, Trails.ParkName FROM Trails inner join Parks on Trails.ParkName = Parks.ParkName WHERE Parks.ParkName like concat('%%', '%s', '%%') and Trails.Length >= (select avg(Trails.Length) as avgLength from Trails) group by Trails.ParkName) as q1 NATURAL JOIN (SELECT AVG(Trails.Popularity) as ShortTrailPopularity, Trails.ParkName FROM Trails WHERE  Trails.Length < (select avg(Trails.Length) as avgLength from Trails) group by Trails.ParkName) as q2 NATURAL JOIN (SELECT Parks.ParkName, count(ParkBiodiversity.Biodiversity) AS NativeCount FROM Parks INNER JOIN ParkBiodiversity ON Parks.ParkName = ParkBiodiversity.Park WHERE ParkBiodiversity.Nativeness = 'Native' GROUP BY ParkName) as q3 NATURAL JOIN Parks;"%user_in
         
-
     # runs just native count query
     elif select_q1 == "1":
         query = "SELECT Parks.ParkName, count(ParkBiodiversity.Biodiversity) AS NativeCount FROM Parks INNER JOIN ParkBiodiversity ON Parks.ParkName = ParkBiodiversity.Park WHERE ParkBiodiversity.Nativeness = 'Native' GROUP BY ParkName ORDER BY NativeCount desc;"    
     # runs just trail popularity query
     elif select_q2 == "1":
         query = "SELECT * FROM(SELECT AVG(Trails.Popularity) as longTrailPopularity, Trails.ParkName FROM Trails WHERE Trails.Length >= (select avg(Trails.Length) as avgLength from Trails) group by Trails.ParkName) as q1 NATURAL JOIN (SELECT AVG(Trails.Popularity) as ShortTrailPopularity, Trails.ParkName FROM Trails WHERE Trails.Length < (select avg(Trails.Length) as avgLength from Trails) group by Trails.ParkName) as q2;"
+    # Add or delete a user
+    elif create_user != '' and create_pass != '':
+      if del_user == "1":
+        delete_query = "delete from Users where Username = '%s'"%create_user
+        mycursor.execute(delete_query)
+        delete_query = "delete from FavoriteTrails where Username = '%s'"%create_user
+        mycursor.execute(delete_query)
+      else :
+        insert_query = "insert into Users values('%s', '%s')"%(create_user, create_pass)
+        print(insert_query)
+        mycursor.execute(insert_query)
+      mydb.commit()
+      query =  "select * from Users"
+    # Add or delete a favorite trail
+    elif add_trail_user != '' and add_trail_name != '':
+      if del_trail == "1":
+        delete_query = "delete from FavoriteTrails where Username = '%s' and TrailName = '%s'"%(add_trail_user, add_trail_name)
+        mycursor.execute(delete_query)
+      else :
+        insert_query = "insert into FavoriteTrails values('%s', '%s', 0)"%(add_trail_user, add_trail_name)
+        mycursor.execute(insert_query)
+      mydb.commit()  
+      query = "select * from FavoriteTrails where Username like concat('%%', '%s', '%%')"%add_trail_user
+    # this will execute any sql query, useful for debugging
+    elif custom_query != '':
+        if ("select" in custom_query.lower()):
+          query = custom_query
+        else:
+          mycursor.execute(custom_query)
+          mydb.commit()
+          query = ''    
+    # Update FavoriteTrails table's Visited field to 1 instead of 0 to mark as visited for specified user
     else:
-        # Update FavoriteTrails table's Visited field to 1 instead of 0 to mark as visited for specified user
         update_query = "update FavoriteTrails set Visited = 1 where TrailName = '" + trailname_in + "' and Username = '" + username_in + "'"
         print (update_query)
         mycursor.execute(update_query)
--- JAAV_Project/static/js/script.js
+++ JAAV_Project/static/js/script.js
@@ -11,9 +11,21 @@ $( document ).ready(function() {
         update_fav_trail_usrnm = document.getElementById('sql-updateFavTrail-username').value;
         update_fav_trail_trlnm = document.getElementById('sql-updateFavTrail-trail').value;
         custom_query = document.getElementById('sql-query-input').value;
+        create_user = document.getElementById("create-user").value;
+        create_pass = document.getElementById("create-pass").value;
+        add_trail_user = document.getElementById("add-trail-user").value;
+        add_trail_name = document.getElementById("add-trail-name").value;
         select_query_1 = 0;
         select_query_2 = 0;
-
+        del_user = 0;
+        del_trail = 0;
+        
+        if (document.getElementById("delete-user").checked) {
+          del_user = 1;
+        }
+        if (document.getElementById("delete-trail").checked) {
+          del_trail = 1;
+        }
         if (q1_result.checked) {
             select_query_1 = 1;
         }
@@ -27,7 +39,13 @@ $( document ).ready(function() {
             select_query_2: select_query_2,
             update_fav_trail_usrnm: update_fav_trail_usrnm,
             update_fav_trail_trlnm: update_fav_trail_trlnm,
-            custom_query: custom_query
+            custom_query: custom_query,
+            del_trail: del_trail,
+            del_user: del_user,
+            create_user: create_user,
+            create_pass: create_pass,
+            add_trail_user: add_trail_user,
+            add_trail_name: add_trail_name
         }, function(result, status){
             result_data = result['data']
 
